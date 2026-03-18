9443dba415e761de3b987a9545244e6d559f5c02
Preet Chauhan <preetchauhan421@gmail.com>
2026-03-18 16:32:50 +0530

    fix: disabled deletion protection on alb

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -1,12 +1,13 @@
 module "alb-nlb" {
-  source                = "PreetChauhan111/alb-nlb/pc"
-  version               = "1.1.0"
-  name                  = local.alb_name
-  tags                  = local.alb_tags
-  vpc_id                = module.vpc.vpc_id
-  subnets               = module.vpc.public_subnets
-  create_security_group = false
-  security_groups       = [module.alb-sg.security_group_id]
+  source                     = "PreetChauhan111/alb-nlb/pc"
+  version                    = "1.1.0"
+  name                       = local.alb_name
+  tags                       = local.alb_tags
+  vpc_id                     = module.vpc.vpc_id
+  subnets                    = module.vpc.public_subnets
+  create_security_group      = false
+  security_groups            = [module.alb-sg.security_group_id]
+  enable_deletion_protection = false
   listeners = {
     http = {
       port     = 80
@@ -18,10 +19,10 @@ module "alb-nlb" {
   }
   target_groups = {
     app = {
-      name        = local.target_group_name
-      protocol    = "HTTP"
-      port        = 80
-      target_type = "instance"
+      name              = local.target_group_name
+      protocol          = "HTTP"
+      port              = 80
+      target_type       = "instance"
       create_attachment = false
 
       health_check = {
