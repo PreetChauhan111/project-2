832df9a0c8abf062bc9e0b23c282c05c010b39a2
Preet Chauhan <preetchauhan421@gmail.com>
2026-03-18 15:48:53 +0530

    fix: sg.tf

--- terraform/sg.tf
+++ terraform/sg.tf
@@ -17,7 +17,7 @@ module "ec2-sg" {
   vpc_id      = module.vpc.vpc_id
   description = "Allow HTTP from ALB security group only"
   ingress_with_source_security_group_id = [
-    "restrict" = {
+    {
       rule                     = "http-80-tcp"
       source_security_group_id = module.alb-sg.security_group_id
     }
